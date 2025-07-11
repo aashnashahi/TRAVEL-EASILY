951762828bc5ebe55e55201ad384f510756cf241
pratiksha bahuguna <129508103+Pratiksha-25@users.noreply.github.com>
2025-07-11 17:56:31 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = () => {
-  fetch(`${process.env.REACT_APP_API_URL}/api/hello`)
-    .then(res => res.json())
-    .then(data => console.log(data));
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}/api/hello`)
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.error("Failed to connect to backend:", err));
+  }, []);
 
   return <h1>Frontend Connected to Backend</h1>;
 };
